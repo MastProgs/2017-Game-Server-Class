590e1537d8768eb5db2af1a157912b76d444182a
Uzchowall <khjkhj2804@naver.com>
2017-03-09 17:49:57 +0900

    기본 클라이언트 체스판 8 * 8 만들기

---
+++ Client/Client/worldmap.h
@@ -0,0 +1,37 @@
+#pragma once
+#include"stdafx.h"
+
+using position = struct Position
+{
+	int x{ 0 };
+	int y{ 0 };
+};
+
+class Player
+{
+public:
+	Player() { pos.x = 4; pos.y = 4; };
+	~Player() {};
+
+	void move_right() { if (!(pos.x > MAX_MAP_SIZE - 2)) { ++pos.x; } };
+	void move_left() { if (!(pos.x < 1)) { --pos.x; } };
+	void move_up() { if (!(pos.y < 1)) { --pos.y; } };
+	void move_down() { if (!(pos.y > MAX_MAP_SIZE - 2)) { ++pos.y; } };
+
+	position get_pos() const { return pos; }
+
+private:
+	position pos;
+};
+
+class worldmap {
+public:
+	worldmap() {};
+	~worldmap() {};
+
+	Player player;
+private:
+	int map[MAX_MAP_SIZE][MAX_MAP_SIZE]{ 0 };
+};
+
+static worldmap g_map;
